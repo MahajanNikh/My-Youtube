a00c41803388870a01abf53a08e765d302a4ce24
Nikhil Mahajan <mahajan12647@gmail.com>
2023-12-03 15:35:22 +0530

    userModel & videoModel completed

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary Url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary Url
+      reqyired: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudinary Url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
